66110057006372732cfbf5a3550fd0f7038408f9
HiepNP0901 <ngphuchiep203@gmail.com>
2026-06-02 05:37:52 +0700

    fix: remove duplicate DTO files

--- core/firebase/src/main/java/com/drs/auralife/data/remote/firebase/model/BannerDto.kt
+++
@@ -1,6 +0,0 @@
-package com.drs.auralife.data.remote.firebase.model.banner
-
-data class BannerDto(
-    val imageUrl: String,
-    val filmSlug: String,
-)
--- core/firebase/src/main/java/com/drs/auralife/data/remote/firebase/model/library/LibraryDto.kt
+++
@@ -1,7 +0,0 @@
-package com.drs.auralife.data.remote.firebase.model.library
-
-data class LibraryDto(
-    var name: String,
-    val posterUrl: String,
-    val listFilm: MutableList<FilmLibrary>,
-)
