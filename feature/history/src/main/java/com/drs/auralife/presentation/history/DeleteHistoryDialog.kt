39a87fcd27801e80c15ebdae4579b7862fe479b4
HiepNP0901 <ngphuchiep203@gmail.com>
2026-06-02 08:14:19 +0700

    refactor: remove duplicated drawables from features, keep only code-referenced ones

--- feature/history/src/main/java/com/drs/auralife/presentation/history/DeleteHistoryDialog.kt
+++ feature/history/src/main/java/com/drs/auralife/presentation/history/DeleteHistoryDialog.kt
@@ -1,6 +1,5 @@
 package com.drs.auralife.presentation.history
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.TextView
