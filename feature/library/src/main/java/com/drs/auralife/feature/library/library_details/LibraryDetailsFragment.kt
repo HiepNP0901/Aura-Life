6f5625c1f7781ee52bbae004bd7848503e850da9
Nguyen Phuc Hiep <132082516+HiepNP0901@users.noreply.github.com>
2026-06-15 13:42:04 +0700

    Fix: add missing View import in LibraryDetailsFragment (#118)

--- feature/library/src/main/java/com/drs/auralife/feature/library/library_details/LibraryDetailsFragment.kt
+++ feature/library/src/main/java/com/drs/auralife/feature/library/library_details/LibraryDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.drs.auralife.feature.library.library_details
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
